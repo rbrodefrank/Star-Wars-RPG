73ad6a64830c02c90dd26d22fedaef09991a637b
rBrodeFrank <ryanafrank@gmail.com>
2018-11-12 20:16:57 -0600

    added psuedo code and basic html structure

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+
+//Variables
+
+//Character Objects
+    //Character Name
+    //Health Points
+    //Attack Power
+    //Counter Attack Power
+//Function Calls
+
+//Character Select
+
+//Attack Button onClick
+
