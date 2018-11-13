77fe87de8c9f3a9d8b2cb2a082b2aeccbe5afbf4
rBrodeFrank <ryanafrank@gmail.com>
2018-11-13 12:03:44 -0600

    added restart button with functionality, added end game functionality (win/lose), adjusted character stats

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,50 +21,61 @@ var obiwan = {
     characterName: "Obi-Wan Kenobi",
     healthPoints: 120,
     attackPower: 8,
-    counterAttack: 10,
+    counterAttack: 20,
     divID: "",
 }
 characters.push(obiwan);
 
 var luke = {
     characterName: "Luke Skywalker",
-    healthPoints: 100,
-    attackPower: 10,
-    counterAttack: 5,
+    healthPoints: 180,
+    attackPower: 4,
+    counterAttack: 25,
     divID: "",
 }
 characters.push(luke);
 
 var maul = {
     characterName: "Darth Maul",
-    healthPoints: 180,
-    attackPower: 5,
-    counterAttack: 25,
+    healthPoints: 100,
+    attackPower: 15,
+    counterAttack: 5,
     divID: "",
 }
 characters.push(maul);
 
 var sidious = {
     characterName: "Darth Sidious",
-    healthPoints: 150,
-    attackPower: 6,
-    counterAttack: 20,
+    healthPoints: 140,
+    attackPower: 10,
+    counterAttack: 15,
     divID: "",
 }
 characters.push(sidious);
 
+//Hide restart button
+$("#restart-btn").hide();
+
 //Function Declarations
 function restart() {
+    //reset variables
     obiwan.healthPoints = 120;
-    luke.healthPoints = 100;
-    maul.healthPoints = 180;
-    sidious.healthPoints = 150;
+    luke.healthPoints = 180;
+    maul.healthPoints = 100;
+    sidious.healthPoints = 140;
     damage = 0;
     characterSelect = true;
     fighting = false;
     enemySelect = false;
     selectedCharacter = "";
     opponent = "";
+    opponentsDefeated = 0;
+    $("#restart-btn").hide();
+
+    $("#obiwan-div div:last-child").text(obiwan.healthPoints);
+    $("#luke-div div:last-child").text(luke.healthPoints);
+    $("#maul-div div:last-child").text(maul.healthPoints);
+    $("#sidious-div div:last-child").text(sidious.healthPoints);
     //move all characters to select area
     for (var i = 0; i < characters.length; i++) {
         characters[i].divID.show();
@@ -84,11 +95,10 @@ maul.divID = $("#maul-div");
 
 $("#character-select").append("<div id='sidious-div'></div>");
 sidious.divID = $("#sidious-div");
-
 // console.log(characters);
 
+//Add to character elements
 for (var i = 0; i < characters.length; i++) {
-    // console.log(characters[i]);
     // console.log(characters[i]);
     characters[i].divID.addClass("character border text-center p-2");
     characters[i].divID.attr("object", characters[i].characterName);
@@ -114,7 +124,7 @@ $(".character").on("click", function () {
         }
         enemySelect = true;
         characterSelect = false;
-    } else if (enemySelect) {
+    } else if (enemySelect && $(this).attr("object") != selectedCharacter.characterName) {
         var defender = $(this).attr("object");
         for (var i = 0; i < characters.length; i++) {
             if (characters[i].characterName === defender) {
@@ -145,7 +155,9 @@ $("#attack").on("click", function () {
             $("#message").text("You defeated " + opponent.characterName + ", you can choose to fight another enemy.");
             if(opponentsDefeated >= 3) {
                 //you won!
+                fighting = false;
                 $("#message").text("You Won! Game Over!");
+                $("#restart-btn").show();
             }
         } else {
             $("#defender div:last-child div:last-child").text(opponent.healthPoints);
@@ -154,11 +166,24 @@ $("#attack").on("click", function () {
             $("#character div div:last-child").text(selectedCharacter.healthPoints);
             if(selectedCharacter.healthPoints <= 0){
                 //game over!
+                fighting = false;
+                $("#message").text("You Lost! Game Over!");
+                $("#restart-btn").show();
             }
         }
     } else if (opponentsDefeated >= 3) {
         $("#message").text("You Won! Game Over!");
+        $("#restart-btn").show();
+    } else if (selectedCharacter.healthPoints <= 0) {
+        $("#message").text("You Lost! Game Over!");
+        $("#restart-btn").show();
     }else {
         $("#message").text("No enemy here.");
     }
 });
+
+//Restart Button onClick
+$("#restart-btn").on("click", function () {
+    restart();
+});
+
